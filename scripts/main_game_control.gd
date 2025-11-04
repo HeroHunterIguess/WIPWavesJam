feef6c9bc04fbac9a15ce4dcbf71d961171ac750
HeroHunter <abramscarter1999@gmail.com>
2025-11-04 16:46:35 -0500

    Create enemy spawning function

--- scripts/main_game_control.gd
+++ scripts/main_game_control.gd
@@ -1,1 +1,18 @@
 extends Node2D
+
+# preload enemy scenes
+const BasicEnemyPreload = preload("res://scenes/objects/enemy.tscn")
+
+
+# spawn enemy at inputted coordinates
+func spawnEnemyAtCoords(x,y):
+	var BasicEnemy = BasicEnemyPreload.instantiate()
+	add_child(BasicEnemy)
+	BasicEnemy.global_position = Vector2(x,y)
+
+# runs the frame this script/scene is loaded
+func _ready(): 
+	# spawn some test enemies
+	spawnEnemyAtCoords(100,100)
+	spawnEnemyAtCoords(500,500)
+	spawnEnemyAtCoords(350,200)
